8636b7319232feb19b7da2a1c029e32075bf857a
Aaron McLeod <sircoolguy99@gmail.com>
2014-11-05 21:36:36 -0500

    changed player geometry

--- player.js
+++ player.js
@@ -1,8 +1,22 @@
 var Player = (function () {
   function Player (parent) {
-    var geo = new THREE.BoxGeometry(1, 2, 1);
-    var material = new THREE.MeshLambertMaterial({ color: 0xffff00 });
-    this.mesh = new THREE.Mesh(geo, material);
+    var verticesOfCube = [
+      -1,-1,-1,    1,-1,-1,    1, 1,-1,    -1, 1,-1,
+      -1,-1, 1,    1,-1, 1,    1, 1, 1,    -1, 1, 1,
+    ];
+
+    var indicesOfFaces = [
+      2,1,0,    0,3,2,
+      0,4,7,    7,3,0,
+      0,1,5,    5,4,0,
+      1,2,6,    6,5,1,
+      2,3,7,    7,6,2,
+      4,5,6,    6,7,4
+    ];
+
+    var geometry = new THREE.PolyhedronGeometry( verticesOfCube, indicesOfFaces, 1, 1 );
+    var mat = new THREE.MeshPhongMaterial({ color: 0xffff00 });
+    this.mesh = new THREE.Mesh(geometry, mat);
     this.mesh.position.z = -parent.position.y + 1;
     this.mesh.rotation.x = Math.PI * 0.5;
     parent.add(this.mesh);
