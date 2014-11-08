db002143ae5efbee12dc7a1be43da77979b9fddf
Aaron McLeod <sircoolguy99@gmail.com>
2014-11-07 20:51:07 -0500

    trying to fix things

--- player.js
+++ player.js
@@ -4,13 +4,13 @@ var Player = (function () {
 
     var mat = Physijs.createMaterial(
       new THREE.MeshPhongMaterial({ color: 0xffff00 }),
-      0.8,
-      0
+      8,
+      10
     );
     this.mesh = new Physijs.BoxMesh(geometry, mat);
     this.mesh.position.y = 0.5;
-    this.mesh.setAngularVelocity(new THREE.Vector3(0, 0, 0));
     parent.add(this.mesh);
+    var _this = this;
   }
 
   return Player;
