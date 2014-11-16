b0d86117a39add422437f0c640d4c81bb204f644
Aaron McLeod <sircoolguy99@gmail.com>
2014-11-16 15:50:05 -0500

    working on physics

--- player.js
+++ player.js
@@ -1,6 +1,4 @@
 var Player = (function () {
-  var MAX_VEL = 5;
-  var MAX_VEL_NEG = -MAX_VEL;
   var notMovingVector = new THREE.Vector3(0, 0, 0);
   function Player (parent) {
     var geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -35,14 +33,11 @@ var Player = (function () {
       vector.unproject(scene.camera);
 
       var dir = vector.sub(this.mesh.position).normalize();
-      var distance = - this.mesh.position.y / dir.y;
-      dir.multiplyScalar(distance);
-      var xVel = dir.x * -40, zVel = dir.z * -40;
+      // var distance = - this.mesh.position.y / dir.y;
+      // dir.multiplyScalar(distance);
 
-      if (xVel > MAX_VEL) xVel = MAX_VEL;
-      if (zVel > MAX_VEL) zVel = MAX_VEL;
-      if (xVel < MAX_VEL_NEG) xVel = MAX_VEL_NEG;
-      if (zVel < MAX_VEL_NEG) zVel = MAX_VEL_NEG;
+      var xVel = dir.x * 100, zVel = dir.z * 100;
+      console.log(dir);
 
       this.mesh.setLinearVelocity(new THREE.Vector3(xVel, 0, zVel));
     }
